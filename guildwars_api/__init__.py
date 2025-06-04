40c29db44f8a740c2daf8943f2a744f6b16a02d5
AlexAndrasi <lockedplayer@gmail.com>
2025-06-04 18:47:14 +0200

    Bug fixed about remaining bosses, Economy (Coin/Gem) related information added

--- guildwars_api/__init__.py
+++ guildwars_api/__init__.py
@@ -12,10 +12,14 @@ def get_daily_chest_request():
     return requests.get("https://api.guildwars2.com/v2/mapchests")
 
 
+# Economy Related Endpoints
+def get_coin_to_gem_exchange_rate():
+    return requests.get("https://api.guildwars2.com/v2/commerce/exchange/coins?quantity=100000")
+
+def get_gem_to_coin_exchange_rate():
+    return requests.get("https://api.guildwars2.com/v2/commerce/exchange/gems?quantity=100")
+
+
 # Account Related Endpoints
 def get_world_bosses_of_specific_account():
     return requests.get("https://api.guildwars2.com/v2/account/worldbosses", headers={'Authorization': f'Bearer {THIRI_BEARER_TOKEN}'})
-
-
-
-
--- information/information.py
+++ information/information.py
@@ -1,5 +1,7 @@
-from guildwars_api import get_daily_world_boss_request, get_daily_chest_request, get_daily_crafting_request, get_world_bosses_of_specific_account
+from guildwars_api import get_daily_world_boss_request, get_daily_chest_request, get_daily_crafting_request, \
+    get_world_bosses_of_specific_account, get_gem_to_coin_exchange_rate, get_coin_to_gem_exchange_rate
 from datetime import datetime
+
 
 # General Information
 def get_daily_world_boss_list():
@@ -7,7 +9,7 @@ def get_daily_world_boss_list():
 
     formatted_bosses = []
     for boss in daily_world_boss_list:
-        formatted_boss_name = boss.replace("_"," ").title()
+        formatted_boss_name = boss.replace("_", " ").title()
         formatted_bosses.append(formatted_boss_name)
 
     return formatted_bosses
@@ -18,7 +20,7 @@ def get_daily_chest_list():
 
     formatted_chests = []
     for chest in daily_chest_list:
-        formatted_chest_name = chest.replace("_"," ").title()
+        formatted_chest_name = chest.replace("_", " ").title()
         formatted_chests.append(formatted_chest_name)
 
     return formatted_chests
@@ -29,7 +31,7 @@ def get_daily_crafting_list():
 
     formatted_crafts = []
     for craft in daily_crafting_list:
-        formatted_craft_name = craft.replace("_"," ").title()
+        formatted_craft_name = craft.replace("_", " ").title()
         formatted_crafts.append(formatted_craft_name)
 
     return formatted_crafts
@@ -39,26 +41,51 @@ def get_daily_crafting_list():
 def get_remaining_world_bosses(formatted_bosses, get_world_bosses_of_specific_account):
     remaining_world_bosses = []
     eliminated_bosses = get_world_bosses_of_specific_account.json()
-
     for boss in formatted_bosses:
-        if boss.lower() not in [eliminated_boss.lower() for eliminated_boss in eliminated_bosses]:
+        if boss.lower() not in [eliminated_boss.lower().replace("_", " ") for eliminated_boss in eliminated_bosses]:
             remaining_world_bosses.append(boss)
 
     return remaining_world_bosses
 
 
+# Economy Related Information
+def get_coin_to_gem_exchange_rate_details(get_coin_to_gem_exchange_rate):
+    coin_to_gem_calculation = get_coin_to_gem_exchange_rate.json()
+    return coin_to_gem_calculation['coins_per_gem'], coin_to_gem_calculation['quantity']
+
+
+def get_gem_to_coin_exchange_rate_details(get_gem_to_coin_exchange_rate):
+    gem_to_coin_calculation = get_gem_to_coin_exchange_rate.json()
+    return gem_to_coin_calculation['coins_per_gem'], gem_to_coin_calculation['quantity']
+
+
 # Formatter and Runner
-def format_general_daily_information(get_daily_world_boss_list, get_daily_chest_list, get_daily_crafting_list, get_remaining_world_bosses):
+def format_general_daily_information(get_daily_world_boss_list, get_daily_chest_list, get_daily_crafting_list,
+                                     get_remaining_world_bosses, coin_to_gem_amount, coin_to_gem_quantity,
+                                     gem_to_coin_amount, gem_to_coin_quantity):
     print(
         f"The daily objectives to this date: ({datetime.today()}) are the following: \n"
         f"- World boss list: {get_daily_world_boss_list}\n"
         f"  You still have to eliminate {len(get_remaining_world_bosses)} out of {len(get_daily_world_boss_list)} world bosses: {get_remaining_world_bosses}\n"
         f"- Chest list: {get_daily_chest_list}\n"
-        f"- Crafting list: {get_daily_crafting_list}")
+        f"- Crafting list: {get_daily_crafting_list}"
+        f"  \n\nInformation about the current state of the economy:\n"
+        f"- The number of coins you required for a single gem: {coin_to_gem_amount} / The number of gems you get for 100000 coins: {coin_to_gem_quantity}\n"
+        f"- The number of coins you get for a single gem: {gem_to_coin_amount} / The number of coins you get for 100 gems: {gem_to_coin_quantity}"
+    )
 
 
 def run_information_for_players():
-    format_general_daily_information(get_daily_world_boss_list(),
-                                     get_daily_chest_list(),
-                                     get_daily_crafting_list(),
-                                     get_remaining_world_bosses(get_daily_world_boss_list(),get_world_bosses_of_specific_account()))
+    formatted_world_boss_list = get_daily_world_boss_list()
+    formatted_chest_list = get_daily_chest_list()
+    formatted_crafting_list = get_daily_crafting_list()
+    coin_to_gem_amount, coin_to_gem_quantity = get_coin_to_gem_exchange_rate_details(get_coin_to_gem_exchange_rate())
+    gem_to_coin_amount, gem_to_coin_quantity = get_gem_to_coin_exchange_rate_details(get_gem_to_coin_exchange_rate())
+
+    format_general_daily_information(formatted_world_boss_list,
+                                     formatted_chest_list,
+                                     formatted_crafting_list,
+                                     get_remaining_world_bosses(formatted_world_boss_list,
+                                     get_world_bosses_of_specific_account()),
+                                     coin_to_gem_amount, coin_to_gem_quantity,
+                                     gem_to_coin_amount, gem_to_coin_quantity)
